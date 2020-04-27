d3fa0c56ac8bed0b1c1a69501f29eab89adaf076
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-04-27 23:25:03 +0200

    refactor: add One Tap UI for Google Sign In

--- app/src/main/java/io/github/rosariopfernandes/firebaseauthonetap/FirstFragment.kt
+++ app/src/main/java/io/github/rosariopfernandes/firebaseauthonetap/FirstFragment.kt
@@ -1,7 +1,7 @@
 package io.github.rosariopfernandes.firebaseauthonetap
 
-import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,12 +9,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -22,7 +25,9 @@ import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+const val TAG = "FirstFragment"
 const val RC_SIGN_IN = 123
+const val RC_ONE_TAP = 124
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -31,6 +36,11 @@ class FirstFragment : Fragment() {
 
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+
+    // Control whether user declined One Tap UI
+    private var userDeclinedOneTap = false
 
     override fun onStart() {
         super.onStart()
@@ -59,15 +69,48 @@ class FirstFragment : Fragment() {
         googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
         auth = Firebase.auth
 
+        oneTapClient = Identity.getSignInClient(requireContext())
+        signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .build()
+
         view.findViewById<SignInButton>(R.id.btnGoogleSignin).setOnClickListener {
             signIn()
         }
     }
 
     private fun updateUI(currentUser: FirebaseUser?) {
-        currentUser?.let {
+        if (currentUser != null) {
             // If currentUser != null, let's go to the next screen
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        } else {
+            // If the user hasn't already declined to use One Tap sign-in
+            if (!userDeclinedOneTap) {
+                // Check if the user has saved credentials on our app
+                // and display the One Tap UI
+                oneTapClient.beginSignIn(signInRequest)
+                    .addOnSuccessListener { result ->
+                        // This listener will be triggered if the
+                        // user does have saved credentials
+                        try {
+                            startIntentSenderForResult(
+                                result.pendingIntent.intentSender, RC_ONE_TAP,
+                                null, 0, 0, 0, null)
+                        } catch (e: IntentSender.SendIntentException) {
+                            Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                        }
+
+                    }.addOnFailureListener {
+                        // No saved credentials found. Launch the One Tap sign-up flow, or
+                        // do nothing and continue presenting the signed-out UI.
+                    }
+            }
         }
     }
 
@@ -76,10 +119,8 @@ class FirstFragment : Fragment() {
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
-    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
-
-        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+    private fun firebaseAuthWithGoogle(googleIdToken: String) {
+        val credential = GoogleAuthProvider.getCredential(googleIdToken, null)
         auth.signInWithCredential(credential)
             .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
@@ -102,17 +143,43 @@ class FirstFragment : Fragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                firebaseAuthWithGoogle(account)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
-                // ...
+        when (requestCode) {
+            // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+            RC_SIGN_IN -> {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w(TAG, "Google sign in failed", e)
+                    // ...
+                }
+            }
+            // Result returned from launching the Intent from startIntentSenderForResult(...)
+            RC_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    // This credential contains a googleIdToken which
+                    // we can use to authenticate with FirebaseAuth
+                    credential.googleIdToken?.let {
+                        firebaseAuthWithGoogle(it)
+                    }
+                } catch (e: ApiException) {
+                    when (e.statusCode) {
+                        CommonStatusCodes.CANCELED -> {
+                            // The user closed the dialog
+                            userDeclinedOneTap = true
+                        }
+                        CommonStatusCodes.NETWORK_ERROR -> {
+                            // No Internet connection ?
+                        }
+                        else -> {
+                            // Some other error
+                        }
+                    }
+                }
             }
         }
     }
