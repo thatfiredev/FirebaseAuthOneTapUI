eff182cce4197ffdbc64bba345bd0f8984333624
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-04-30 00:16:38 +0200

    refactor: add One Tap UI for Email/Password authentication

--- app/src/main/java/io/github/rosariopfernandes/firebaseauthonetap/FirstFragment.kt
+++ app/src/main/java/io/github/rosariopfernandes/firebaseauthonetap/FirstFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
@@ -19,6 +21,7 @@ import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -42,6 +45,10 @@ class FirstFragment : Fragment() {
     // Control whether user declined One Tap UI
     private var userDeclinedOneTap = false
 
+    // UI elements
+    private lateinit var inputEmail: TextInputLayout
+    private lateinit var inputPassword: TextInputLayout
+
     override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
@@ -60,6 +67,9 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        inputEmail = view.findViewById(R.id.tilEmail)
+        inputPassword = view.findViewById(R.id.tilPassword)
+
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
@@ -71,6 +81,9 @@ class FirstFragment : Fragment() {
 
         oneTapClient = Identity.getSignInClient(requireContext())
         signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
             .setGoogleIdTokenRequestOptions(
                 BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
                     .setSupported(true)
@@ -80,8 +93,23 @@ class FirstFragment : Fragment() {
                     .build())
             .build()
 
+        // Click listeners for the buttons
         view.findViewById<SignInButton>(R.id.btnGoogleSignin).setOnClickListener {
             signIn()
+        }
+        view.findViewById<Button>(R.id.btnSignIn).setOnClickListener {
+            val email = inputEmail.editText?.text.toString()
+            val password = inputPassword.editText?.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                signInWithPassword(email, password)
+            }
+        }
+        view.findViewById<Button>(R.id.btnSignUp).setOnClickListener {
+            val email = inputEmail.editText?.text.toString()
+            val password = inputPassword.editText?.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                signUpWithPassword(email, password)
+            }
         }
     }
 
@@ -106,9 +134,10 @@ class FirstFragment : Fragment() {
                             Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
                         }
 
-                    }.addOnFailureListener {
+                    }.addOnFailureListener { e ->
                         // No saved credentials found. Launch the One Tap sign-up flow, or
                         // do nothing and continue presenting the signed-out UI.
+                        Log.e(TAG, "No saved credentials: ${e.localizedMessage}")
                     }
             }
         }
@@ -117,6 +146,37 @@ class FirstFragment : Fragment() {
     private fun signIn() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    // Sign in to an existing password account
+    private fun signInWithPassword(email: String, password: String) {
+        Log.e(TAG, "Signing in with email '$email' and '$password'")
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener { authResult ->
+                val currentUser = authResult.user
+                updateUI(currentUser)
+            }
+            .addOnFailureListener {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "signInWithEmail:failure", it)
+                Toast.makeText(requireContext(), "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    // Create a new password account
+    private fun signUpWithPassword(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener { authResult ->
+                val currentUser = authResult.user
+                updateUI(currentUser)
+            }
+            .addOnFailureListener {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "createUserWithEmail:failure", it)
+                Toast.makeText(requireContext(), "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun firebaseAuthWithGoogle(googleIdToken: String) {
@@ -165,6 +225,11 @@ class FirstFragment : Fragment() {
                     // we can use to authenticate with FirebaseAuth
                     credential.googleIdToken?.let {
                         firebaseAuthWithGoogle(it)
+                    }
+                    // If the user used email/password, the credential
+                    // should provide the user's email and password
+                    credential.password?.let { password ->
+                        signInWithPassword(credential.id, password)
                     }
                 } catch (e: ApiException) {
                     when (e.statusCode) {
--- app/src/main/java/io/github/rosariopfernandes/firebaseauthonetap/MainActivity.kt
+++ app/src/main/java/io/github/rosariopfernandes/firebaseauthonetap/MainActivity.kt
@@ -2,6 +2,7 @@ package io.github.rosariopfernandes.firebaseauthonetap
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener { _, destination, _ ->
+            supportActionBar?.title = destination.label
+        }
     }
 }
